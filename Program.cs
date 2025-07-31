1d683cd290519aa4f08cb0f25f66cefe3d8a0bdf
KshitijChavan-Stack <chavankshitij0@gmailcom>
2025-07-31 13:29:29 +0530

    "Fix database connection string from DATABASE_URL for Render"

--- Program.cs
+++ Program.cs
@@ -10,8 +10,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AttendanceDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (databaseUrl != null && databaseUrl.StartsWith("postgres://"))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    var connectionString = $"Host={uri.Host};Port={uri.Port};Username={userInfo[0]};Password={userInfo[1]};Database={uri.AbsolutePath.TrimStart('/')};SSL Mode=Require;Trust Server Certificate=true";
+
+    builder.Services.AddDbContext<AttendanceDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
+else
+{
+    throw new Exception("Invalid or missing DATABASE_URL");
+}
+
 
 
 
