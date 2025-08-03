2e4a42fe5b2f248bbb0aa9b6ab8db5b43735b71f
KshitijChavan-Stack <chavankshitij0@gmailcom>
2025-08-03 11:48:35 +0530

    Adding Db url in new class

---
+++ Data/AttendanceDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using AttendanceAPI.Data;
+
+public class AttendanceDbContextFactory : IDesignTimeDbContextFactory<AttendanceDbContext>
+{
+    public AttendanceDbContext CreateDbContext(string[] args)
+    {
+        var connectionString = "Host=dpg-d25ih1re5dus73a52jjg-a;Port=5432;Database=office_logger_db;Username=office_logger_db_user;Password=bkVO99om3XZuCEOa4H85lX18cy4xHC1r";
+
+        var optionsBuilder = new DbContextOptionsBuilder<AttendanceDbContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new AttendanceDbContext(optionsBuilder.Options);
+    }
+}
--- Models/user.cs
+++ Models/user.cs
@@ -2,10 +2,12 @@
 {
     public class user
     {
+        public Guid UserId { get; set; } = Guid.NewGuid();
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
     }
 }
 // This class represents a user in the system with properties for Id, Name, and Email.
